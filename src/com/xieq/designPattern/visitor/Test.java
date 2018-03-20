32976ad374d33192c0196452032d856732828fd1
adamXie <318711439@qq.com>
2018-03-20 18:03:43 +0800

    测试添加文件

---
+++ src/com/xieq/designPattern/visitor/Test.java
@@ -0,0 +1,15 @@
+package com.xieq.designPattern.visitor;
+
+/**
+ * <p>文件名称: ${FILE_NAME}</p>
+ * <p>文件描述: 用户管理实现类</p>
+ * <p>版权所有: 版权所有(C)2017</p>
+ * <p>公 司: 深圳竹云科技有限公司</p>
+ * <p>内容摘要: 主要包含了用户的增删查改启用禁用</p>
+ * <p>其他说明: 无</p>
+ * <p>完成日期: 2018/3/20 0020</p>
+ *
+ * @author xieq
+ */
+public class Test {
+}
